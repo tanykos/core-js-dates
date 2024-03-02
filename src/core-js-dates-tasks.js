f103630d8d922ff8f9317a49817f908a1a01dd3e
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2024-03-02 20:13:11 +0100

    fix: replace toLocaleTimeString in task getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -32,7 +32,10 @@ function dateToTimestamp(date) {
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
 function getTime(date) {
-  return date.toLocaleTimeString();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
