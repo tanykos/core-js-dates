a73fe9396b2e7c62384ba54912f39c65e6b1906c
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2024-03-05 11:53:59 +0100

    feat: resolve tasks getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -238,14 +238,8 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(date) {
-  const startYear = new Date(Date.UTC(date.getFullYear(), 0, 1));
-  const dayOfWeek = startYear.getUTCDay();
-  const dayMsec = 24 * 60 * 60 * 1000;
-  const daysToFullWeek = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
-  const diff = Math.floor((date - startYear) / dayMsec) + 2;
-
-  return Math.ceil((diff + daysToFullWeek) / 7);
+function getWeekNumberByDate(/* date */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -259,8 +253,41 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const currentYear = date.getUTCFullYear();
+  const currentMonth = date.getUTCMonth();
+  const currentDay = date.getUTCDate();
+  let resMonth = currentMonth;
+  let resYear = currentYear;
+
+  if (currentDay > 13) {
+    if (currentMonth === 11) {
+      resYear = currentYear + 1;
+      resMonth = 0;
+    } else {
+      resMonth = currentMonth + 1;
+    }
+  }
+
+  const resDate = new Date(date);
+
+  resDate.setUTCDate(13);
+  resDate.setUTCMonth(resMonth);
+  resDate.setUTCFullYear(resYear);
+
+  while (resDate.getUTCDay() !== 5) {
+    resMonth += 1;
+    resDate.setUTCMonth(resMonth);
+    resDate.setUTCDate(13);
+
+    if (resMonth === 11) {
+      resYear += 1;
+      resDate.setUTCFullYear(resYear);
+      resMonth = -1;
+    }
+  }
+
+  return new Date(resYear, resMonth, 13);
 }
 
 /**
